d6bf27fd3d1ea36b00010e24140eb7601f63771f
Vyacheslav Tuev <vyacheslavtuev@192.168.0.100>
2019-11-02 15:55:42 +0300

    fix 222

--- src/table/Table.js
+++ src/table/Table.js
@@ -1,13 +1,60 @@
 import React from 'react';
+import './Table.css';
 
 function Table() {
 	return (
-		<div className="App">
-			<header className="App-header">
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-			</header>
+		<div className="mainDiv">
+			<table className="tableMain">
+				<thead>
+					<tr>
+						<th>
+							<div className="header">Периоды</div>
+						</th>
+						<th>
+							<div className="header">Ряды</div>
+						</th>
+						<th>
+							<div className="header">I</div>
+						</th>
+						<th>
+							<div className="header">II</div>
+						</th>
+						<th>
+							<div className="header">III</div>
+						</th>
+						<th>
+							<div className="header">IV</div>
+						</th>
+						<th>
+							<div className="header">V</div>
+						</th>
+						<th>
+							<div className="header">VI</div>
+						</th>
+						<th>
+							<div className="header">VII</div>
+						</th>
+						<th>
+							<div className="header">VIII</div>
+						</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>I</td>
+						<td>1</td>
+						<td>Водород</td>
+						<td />
+						<td />
+						<td />
+						<td />
+						<td />
+						<td />
+						<td />
+						<td />
+					</tr>
+				</tbody>
+			</table>{' '}
 		</div>
 	);
 }
