4a93a423eb0116d7282eb4c1a61c846a95c5e695
Vyacheslav Tuev <vyacheslavtuev@192.168.0.100>
2019-11-02 16:25:04 +0300

    fix rows

--- src/table/Table.js
+++ src/table/Table.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './Table.css';
 
+const arr = [ 1, 2, 3, 4, 5, 6, 7, 8 ];
+
 function Table() {
 	return (
 		<div className="mainDiv">
@@ -8,10 +10,10 @@ function Table() {
 				<thead>
 					<tr>
 						<th>
-							<div className="header">Периоды</div>
+							<div className="header0">Периоды</div>
 						</th>
 						<th>
-							<div className="header">Ряды</div>
+							<div className="header1">Ряды</div>
 						</th>
 						<th>
 							<div className="header">I</div>
@@ -41,20 +43,56 @@ function Table() {
 				</thead>
 				<tbody>
 					<tr>
-						<td>I</td>
+						<td>
+							<div className="rowPeriods">I</div>
+						</td>
 						<td>1</td>
-						<td>Водород</td>
-						<td />
-						<td />
-						<td />
-						<td />
-						<td />
-						<td />
-						<td />
-						<td />
+						{arr.map((item) => <td>{item}</td>)}
+					</tr>
+					<tr>
+						<td>
+							<div className="rowPeriods">II</div>
+						</td>
+						<td>1</td>
+						{arr.map((item) => <td>{item}</td>)}
+					</tr>
+					<tr>
+						<td>
+							<div className="rowPeriods">III</div>
+						</td>
+						<td>1</td>
+						{arr.map((item) => <td>{item}</td>)}
+					</tr>
+					<tr>
+						<td>
+							<div className="rowPeriods">IV</div>
+						</td>
+						<td>1</td>
+						{arr.map((item) => <td>{item}</td>)}
+					</tr>
+					<tr>
+						<td>
+							<div className="rowPeriods">V</div>
+						</td>
+						<td>1</td>
+						{arr.map((item) => <td>{item}</td>)}
+					</tr>
+					<tr>
+						<td>
+							<div className="rowPeriods">VI</div>
+						</td>
+						<td>1</td>
+						{arr.map((item) => <td>{item}</td>)}
+					</tr>
+					<tr>
+						<td>
+							<div className="rowPeriods">VII</div>
+						</td>
+						<td>1</td>
+						{arr.map((item) => <td>{item}</td>)}
 					</tr>
 				</tbody>
-			</table>{' '}
+			</table>
 		</div>
 	);
 }
