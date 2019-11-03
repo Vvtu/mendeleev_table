b7971900a54edab03845a8026e0cfcb947e2c243
Vyacheslav Tuev <vyacheslavtuev@192.168.0.100>
2019-11-03 11:52:39 +0300

    fix Elements data

--- src/table/Cell.js
+++ src/table/Cell.js
@@ -4,17 +4,35 @@ import './Table.css';
 import Elements from './Elements';
 
 function Cell(props) {
-	const elemInfo = Elements[props.elem] || {};
+	const { elem, setDragedElemKey, setDragOverElemKey, onDragEnd, onDrop } = props;
+	const elemInfo = Elements[elem] || {};
 	const color = elemInfo.color ? ' bColor' + elemInfo.color : undefined;
+
 	return (
-		<div className={color} draggable>
+		<div
+			className={color}
+			draggable
+			onDragStart={() => setDragedElemKey(elem)}
+			onDragEnd={onDragEnd}
+			onDragOver={(e) => {
+				e.preventDefault();
+			}}
+			onDrop={(e) => {
+				e.preventDefault();
+				onDrop(elem);
+			}}
+		>
 			{elemInfo.color === 'Green' ? (
 				<div className="cellWidth">
 					<div className="cellKeyAndNum">
-						<div className="cellKey">{props.elem}</div>
+						<div className="cellKey">{elem}</div>
 						<div>{elemInfo.num}</div>
 					</div>
-					<div className="cellFlexEnd">
+					<div
+						className="cellFlexEnd"
+						onDragEnter={() => setDragOverElemKey(elem)}
+						onDragLeave={() => setDragOverElemKey('')}
+					>
 						<div>{elemInfo.name}</div>
 					</div>
 					<div className="cellFlexStart">
@@ -25,9 +43,13 @@ function Cell(props) {
 				<div className="cellWidth">
 					<div className="cellKeyAndNum">
 						<div>{elemInfo.num}</div>
-						<div className="cellKey">{props.elem}</div>
+						<div className="cellKey">{elem}</div>
 					</div>
-					<div className="cellFlexStart">
+					<div
+						className="cellFlexStart"
+						onDragEnter={() => setDragOverElemKey(elem)}
+						onDragLeave={() => setDragOverElemKey('')}
+					>
 						<div>{elemInfo.name}</div>
 					</div>
 					<div className="cellFlexEnd">
--- src/table/Table.js
+++ src/table/Table.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Cell from './Cell';
 import './Table.css';
@@ -6,234 +6,374 @@ import './Table.css';
 const arr6 = [ 1, 2, 3, 4, 5, 6 ];
 
 function Table() {
+	const [ dragedElemKey, setDragedElemKey ] = useState('');
+	const [ dragOverElemKey, setDragOverElemKey ] = useState('');
+	const [ dropElemKey, setDropElemKey ] = useState('');
+
+	const onDragEnd = () => {
+		console.log('onDragEnd   dragedElemKey = ' + dragedElemKey + '  dragOverElemKey = ' + dragOverElemKey);
+	};
+	const onDrop = (elemKey) => {
+		setDropElemKey(elemKey);
+		console.log('onDrop   elemKey = ' + elemKey);
+	};
+
+	console.log('dragedElemKey = ' + dragedElemKey + '  dragOverElemKey = ' + dragOverElemKey);
+
 	return (
-		<div className="mainDiv">
-			<table className="tableMain">
-				<thead>
-					<tr>
-						<th>
-							<div className="header0">Периоды</div>
-						</th>
-						<th>
-							<div className="header1">Ряды</div>
-						</th>
-						<th>
-							<div className="header">I</div>
-						</th>
-						<th>
-							<div className="header">II</div>
-						</th>
-						<th>
-							<div className="header">III</div>
-						</th>
-						<th>
-							<div className="header">IV</div>
-						</th>
-						<th>
-							<div className="header">V</div>
-						</th>
-						<th>
-							<div className="header">VI</div>
-						</th>
-						<th>
-							<div className="header">VII</div>
-						</th>
-						<th colSpan="4">
-							<div className="header4">VIII</div>
-						</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr>
-						<td>
-							<div className="rowPeriods">I</div>
-						</td>
-						<td>
-							<div className="rowPeriods">1</div>
-						</td>
-						<td>
-							<Cell key="H" elem="H" />
-						</td>
-						{arr6.map((item) => (
-							<td key={item}>
+		<div>
+			<div className="mainDiv">
+				<table className="tableMain">
+					<thead>
+						<tr>
+							<th>
+								<div className="header0">Периоды</div>
+							</th>
+							<th>
+								<div className="header1">Ряды</div>
+							</th>
+							<th>
+								<div className="header">I</div>
+							</th>
+							<th>
+								<div className="header">II</div>
+							</th>
+							<th>
+								<div className="header">III</div>
+							</th>
+							<th>
+								<div className="header">IV</div>
+							</th>
+							<th>
+								<div className="header">V</div>
+							</th>
+							<th>
+								<div className="header">VI</div>
+							</th>
+							<th>
+								<div className="header">VII</div>
+							</th>
+							<th colSpan="4">
+								<div className="header4">VIII</div>
+							</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>
+								<div className="rowPeriods">I</div>
+							</td>
+							<td>
+								<div className="rowPeriods">1</div>
+							</td>
+							<td>
+								<Cell
+									setDragedElemKey={setDragedElemKey}
+									setDragOverElemKey={setDragOverElemKey}
+									onDrop={onDrop}
+									onDragEnd={onDragEnd}
+									key="H"
+									elem="H"
+								/>
+							</td>
+							{arr6.map((item) => (
+								<td key={item}>
+									<div className="cellWidth" />
+								</td>
+							))}
+							<td colSpan="3">
+								<div className="cellWidth3" />
+							</td>
+							<td>
+								<Cell
+									setDragedElemKey={setDragedElemKey}
+									setDragOverElemKey={setDragOverElemKey}
+									onDrop={onDrop}
+									onDragEnd={onDragEnd}
+									key="He"
+									elem="He"
+								/>
+							</td>
+						</tr>
+						<tr>
+							<td>
+								<div className="rowPeriods">II</div>
+							</td>
+							<td>
+								<div className="rowPeriods">2</div>
+							</td>
+							{[ 'Li', 'Be', 'B', 'C', 'N', 'O', 'F' ].map((item) => (
+								<td key={item}>
+									<Cell
+										setDragedElemKey={setDragedElemKey}
+										setDragOverElemKey={setDragOverElemKey}
+										onDrop={onDrop}
+										onDragEnd={onDragEnd}
+										key={item}
+										elem={item}
+									/>
+								</td>
+							))}
+							<td colSpan="3">
+								<div className="cellWidth3" />
+							</td>
+							<td>
+								<Cell
+									setDragedElemKey={setDragedElemKey}
+									setDragOverElemKey={setDragOverElemKey}
+									onDrop={onDrop}
+									onDragEnd={onDragEnd}
+									key="Ne"
+									elem="Ne"
+								/>
+							</td>
+						</tr>
+						<tr>
+							<td>
+								<div className="rowPeriods">III</div>
+							</td>
+							<td>
+								<div className="rowPeriods">3</div>
+							</td>
+							{[ 'Na', 'Mg', 'Al', 'Si', 'P', 'S', 'Cl' ].map((item) => (
+								<td key={item}>
+									<Cell
+										setDragedElemKey={setDragedElemKey}
+										setDragOverElemKey={setDragOverElemKey}
+										onDrop={onDrop}
+										onDragEnd={onDragEnd}
+										key={item}
+										elem={item}
+									/>
+								</td>
+							))}
+							<td colSpan="3">
+								<div className="cellWidth3" />
+							</td>
+							<td>
+								<Cell
+									setDragedElemKey={setDragedElemKey}
+									setDragOverElemKey={setDragOverElemKey}
+									onDrop={onDrop}
+									onDragEnd={onDragEnd}
+									key="Ar"
+									elem="Ar"
+								/>
+							</td>
+						</tr>
+						<tr>
+							<td rowSpan="2">
+								<div className="rowPeriods">IV</div>
+							</td>
+							<td>
+								<div className="rowPeriods">4</div>
+							</td>
+							{[ 'K', 'Ca', 'Sc', 'Ti', 'V', 'Cr', 'Mn', 'Fe', 'Co', 'Ni' ].map((item) => (
+								<td key={item}>
+									<Cell
+										setDragedElemKey={setDragedElemKey}
+										setDragOverElemKey={setDragOverElemKey}
+										onDrop={onDrop}
+										onDragEnd={onDragEnd}
+										key={item}
+										elem={item}
+									/>
+								</td>
+							))}
+							<td>
 								<div className="cellWidth" />
 							</td>
-						))}
-						<td colSpan="3">
-							<div className="cellWidth3" />
-						</td>
-						<td>
-							<Cell key="He" elem="He" />
-						</td>
-					</tr>
-					<tr>
-						<td>
-							<div className="rowPeriods">II</div>
-						</td>
-						<td>
-							<div className="rowPeriods">2</div>
-						</td>
-						{[ 'Li', 'Be', 'B', 'C', 'N', 'O', 'F' ].map((item) => (
-							<td key={item}>
-								<Cell key={item} elem={item} />
+						</tr>
+						<tr>
+							<td>
+								<div className="rowPeriods">5</div>
 							</td>
-						))}
-						<td colSpan="3">
-							<div className="cellWidth3" />
-						</td>
-						<td>
-							<Cell key="Ne" elem="Ne" />
-						</td>
-					</tr>
-					<tr>
-						<td>
-							<div className="rowPeriods">III</div>
-						</td>
-						<td>
-							<div className="rowPeriods">3</div>
-						</td>
-						{[ 'Na', 'Mg', 'Al', 'Si', 'P', 'S', 'Cl' ].map((item) => (
-							<td key={item}>
-								<Cell key={item} elem={item} />
+							{[ 'Cu', 'Zn', 'Ga', 'Ge', 'As', 'Se', 'Br' ].map((item) => (
+								<td key={item}>
+									<Cell
+										setDragedElemKey={setDragedElemKey}
+										setDragOverElemKey={setDragOverElemKey}
+										onDrop={onDrop}
+										onDragEnd={onDragEnd}
+										key={item}
+										elem={item}
+									/>
+								</td>
+							))}
+							<td colSpan="3">
+								<div className="cellWidth3" />
 							</td>
-						))}
-						<td colSpan="3">
-							<div className="cellWidth3" />
-						</td>
-						<td>
-							<Cell key="Ar" elem="Ar" />
-						</td>
-					</tr>
-					<tr>
-						<td rowSpan="2">
-							<div className="rowPeriods">IV</div>
-						</td>
-						<td>
-							<div className="rowPeriods">4</div>
-						</td>
-						{[ 'K', 'Ca', 'Sc', 'Ti', 'V', 'Cr', 'Mn', 'Fe', 'Co', 'Ni' ].map((item) => (
-							<td key={item}>
-								<Cell key={item} elem={item} />
+							<td>
+								<Cell
+									setDragedElemKey={setDragedElemKey}
+									setDragOverElemKey={setDragOverElemKey}
+									onDrop={onDrop}
+									onDragEnd={onDragEnd}
+									key="Kr"
+									elem="Kr"
+								/>
 							</td>
-						))}
-						<td>
-							<div className="cellWidth" />
-						</td>
-					</tr>
-					<tr>
-						<td>
-							<div className="rowPeriods">5</div>
-						</td>
-						{[ 'Cu', 'Zn', 'Ga', 'Ge', 'As', 'Se', 'Br' ].map((item) => (
-							<td key={item}>
-								<Cell key={item} elem={item} />
+						</tr>
+						<tr>
+							<td rowSpan="2">
+								<div className="rowPeriods">V</div>
 							</td>
-						))}
-						<td colSpan="3">
-							<div className="cellWidth3" />
-						</td>
-						<td>
-							<Cell key="Kr" elem="Kr" />
-						</td>
-					</tr>
-					<tr>
-						<td rowSpan="2">
-							<div className="rowPeriods">V</div>
-						</td>
-						<td>
-							<div className="rowPeriods">6</div>
-						</td>
-						{[ 'Rb', 'Sr', 'Y', 'Zr', 'Nb', 'Mo', 'Tc', 'Ru', 'Rh', 'Pd' ].map((item) => (
-							<td key={item}>
-								<Cell key={item} elem={item} />
+							<td>
+								<div className="rowPeriods">6</div>
 							</td>
-						))}
-						<td>
-							<div className="cellWidth" />
-						</td>
-					</tr>
-					<tr>
-						<td>
-							<div className="rowPeriods">7</div>
-						</td>
-						{[ 'Ag', 'Cd', 'In', 'Sn', 'Sb', 'Te', 'I' ].map((item) => (
-							<td key={item}>
-								<Cell key={item} elem={item} />
-							</td>
-						))}
-						<td colSpan="3">
-							<div className="cellWidth3" />
-						</td>
-						<td>
-							<Cell key="Xe" elem="Xe" />
-						</td>
-					</tr>
-					<tr>
-						<td rowSpan="2">
-							<div className="rowPeriods">VI</div>
-						</td>
-						<td>
-							<div className="rowPeriods">8</div>
-						</td>
-						{[ 'Cs', 'Ba', 'La*', 'Hf', 'Ta', 'W', 'Re', 'Os', 'Ir', 'Pt' ].map((item) => (
-							<td key={item}>
-								<Cell key={item} elem={item} />
-							</td>
-						))}
-						<td>
-							<div className="cellWidth" />
-						</td>
-					</tr>
-					<tr>
-						<td>
-							<div className="rowPeriods">9</div>
-						</td>
-						{[ 'Au', 'Hg', 'Tl', 'Pb', 'Bi', 'Po', 'At' ].map((item) => (
-							<td key={item}>
-								<Cell key={item} elem={item} />
-							</td>
-						))}
-						<td colSpan="3">
-							<div className="cellWidth3" />
-						</td>
-						<td>
-							<Cell key="Rn" elem="Rn" />
-						</td>
-					</tr>
-					<tr>
-						<td rowSpan="2">
-							<div className="rowPeriods">VII</div>
-						</td>
-						<td>
-							<div className="rowPeriods">10</div>
-						</td>
-						{[ 'Fr', 'Ra', 'Ac**', 'Rf', 'Db', 'Sg', 'Bh', 'Hs', 'Mt', 'Ds' ].map((item) => (
-							<td key={item}>
-								<Cell key={item} elem={item} />
-							</td>
-						))}
-						<td>
-							<div className="cellWidth" />
-						</td>
-					</tr>
-					<tr>
-						<td>
-							<div className="rowPeriods">11</div>
-						</td>
-						<td>
-							<Cell key="Rg" elem="Rg" />
-						</td>
-						{arr6.map((item) => (
-							<td key={item}>
+							{[ 'Rb', 'Sr', 'Y', 'Zr', 'Nb', 'Mo', 'Tc', 'Ru', 'Rh', 'Pd' ].map((item) => (
+								<td key={item}>
+									<Cell
+										setDragedElemKey={setDragedElemKey}
+										setDragOverElemKey={setDragOverElemKey}
+										onDrop={onDrop}
+										onDragEnd={onDragEnd}
+										key={item}
+										elem={item}
+									/>
+								</td>
+							))}
+							<td>
 								<div className="cellWidth" />
 							</td>
-						))}
-						<td colSpan="4">
-							<div className="cellWidth4" />
-						</td>
-					</tr>
-				</tbody>
-			</table>
+						</tr>
+						<tr>
+							<td>
+								<div className="rowPeriods">7</div>
+							</td>
+							{[ 'Ag', 'Cd', 'In', 'Sn', 'Sb', 'Te', 'I' ].map((item) => (
+								<td key={item}>
+									<Cell
+										setDragedElemKey={setDragedElemKey}
+										setDragOverElemKey={setDragOverElemKey}
+										onDrop={onDrop}
+										onDragEnd={onDragEnd}
+										key={item}
+										elem={item}
+									/>
+								</td>
+							))}
+							<td colSpan="3">
+								<div className="cellWidth3" />
+							</td>
+							<td>
+								<Cell
+									setDragedElemKey={setDragedElemKey}
+									setDragOverElemKey={setDragOverElemKey}
+									onDrop={onDrop}
+									onDragEnd={onDragEnd}
+									key="Xe"
+									elem="Xe"
+								/>
+							</td>
+						</tr>
+						<tr>
+							<td rowSpan="2">
+								<div className="rowPeriods">VI</div>
+							</td>
+							<td>
+								<div className="rowPeriods">8</div>
+							</td>
+							{[ 'Cs', 'Ba', 'La*', 'Hf', 'Ta', 'W', 'Re', 'Os', 'Ir', 'Pt' ].map((item) => (
+								<td key={item}>
+									<Cell
+										setDragedElemKey={setDragedElemKey}
+										setDragOverElemKey={setDragOverElemKey}
+										onDrop={onDrop}
+										onDragEnd={onDragEnd}
+										key={item}
+										elem={item}
+									/>
+								</td>
+							))}
+							<td>
+								<div className="cellWidth" />
+							</td>
+						</tr>
+						<tr>
+							<td>
+								<div className="rowPeriods">9</div>
+							</td>
+							{[ 'Au', 'Hg', 'Tl', 'Pb', 'Bi', 'Po', 'At' ].map((item) => (
+								<td key={item}>
+									<Cell
+										setDragedElemKey={setDragedElemKey}
+										setDragOverElemKey={setDragOverElemKey}
+										onDrop={onDrop}
+										onDragEnd={onDragEnd}
+										key={item}
+										elem={item}
+									/>
+								</td>
+							))}
+							<td colSpan="3">
+								<div className="cellWidth3" />
+							</td>
+							<td>
+								<Cell
+									setDragedElemKey={setDragedElemKey}
+									setDragOverElemKey={setDragOverElemKey}
+									onDrop={onDrop}
+									onDragEnd={onDragEnd}
+									key="Rn"
+									elem="Rn"
+								/>
+							</td>
+						</tr>
+						<tr>
+							<td rowSpan="2">
+								<div className="rowPeriods">VII</div>
+							</td>
+							<td>
+								<div className="rowPeriods">10</div>
+							</td>
+							{[ 'Fr', 'Ra', 'Ac**', 'Rf', 'Db', 'Sg', 'Bh', 'Hs', 'Mt', 'Ds' ].map((item) => (
+								<td key={item}>
+									<Cell
+										setDragedElemKey={setDragedElemKey}
+										setDragOverElemKey={setDragOverElemKey}
+										onDrop={onDrop}
+										onDragEnd={onDragEnd}
+										key={item}
+										elem={item}
+									/>
+								</td>
+							))}
+							<td>
+								<div className="cellWidth" />
+							</td>
+						</tr>
+						<tr>
+							<td>
+								<div className="rowPeriods">11</div>
+							</td>
+							<td>
+								<Cell
+									setDragedElemKey={setDragedElemKey}
+									setDragOverElemKey={setDragOverElemKey}
+									onDrop={onDrop}
+									onDragEnd={onDragEnd}
+									key="Rg"
+									elem="Rg"
+								/>
+							</td>
+							{arr6.map((item) => (
+								<td key={item}>
+									<div className="cellWidth" />
+								</td>
+							))}
+							<td colSpan="4">
+								<div className="cellWidth4" />
+							</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+			<div>
+				<div>{'dragedElemKey = ' + dragedElemKey}</div>
+				<div>{'dragOverElemKey = ' + dragOverElemKey}</div>
+				<div>{'dropElemKey = ' + dropElemKey}</div>
+			</div>
 		</div>
 	);
 }
